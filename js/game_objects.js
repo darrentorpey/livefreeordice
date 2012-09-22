8bbdce3e8746fba665adaa2853872672e21bed27
Darren Torpey <darren.torpey@gmail.com>
2012-09-22 15:21:32 -0400

    First commit; basic Zone attacking each other

---
+++ js/game_objects.js
@@ -0,0 +1,109 @@
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+}
+
+
+PLAYERS = {
+  NIL:    '',
+  BLUE:   'Blue',
+  RED:    'Red',
+  YELLOW: 'Yellow',
+  GREEN:  'Green'
+}
+
+/*
+ * =====================
+ * | Live Free or Dice |
+ * ---------------------
+ * written by Darren Torpey & Greg Kinneman
+ * 9/22/12
+ * BostonFIG Adobe Game Jam - www.bostonfig.com
+*/
+
+/* *******************
+  Zone
+    An area you control
+*/
+var Zone = Klass.extend({
+  init: function(params) {
+    _.defaults(params, {
+      x:     0,
+      y:     0,
+      power: 1,
+      owner: PLAYERS.NIL,
+    })
+
+    this.x     = params.x,
+    this.y     = params.y,
+    this.power = params.power,
+    this.owner = params.owner;
+  },
+
+  quickview: function() {
+    return '||' + this.owner + ' (P:' + this.power + ')' + ' [' + this.x + ', ' + this.y + ']' + '||';
+  },
+
+  // Attack this zone with the given attacker
+  // Returns: whether the attack was successful
+  attackWith: function(attacker) {
+    notice('-----');
+    notice(attacker.quickview() + ' attacks ' + this.quickview());
+
+    debug('Attacker: ' + attacker.quickview());
+    debug('Defender: ' + this.quickview());
+
+    var attackerTotal = attacker.rollUp();
+    debug('Attacker rolls: ' + attackerTotal);
+    var defenderTotal = this.rollUp();
+    debug('Defender rolls: ' + defenderTotal);
+
+    var victory = attackerTotal > defenderTotal;
+    if (victory) {
+      notice('Attacker wins!');
+      this.processInvasion(attacker);
+    } else {
+      notice('Defender wins!');
+    }
+    debug('Attacker: ' + attacker.quickview());
+    debug('Defender: ' + this.quickview());
+  },
+
+  processInvasion: function(attacker) {
+    this.owner = attacker.owner;
+    this.power = attacker.power - 1;
+    attacker.power = 1;
+  },
+
+  rollUp: function() {
+    var totalRoll = 0;
+    _(this.power).times(function() {
+      // console.log(i);
+      var roll = [1,2,3,4,5,6].randomElement();
+      totalRoll += roll;
+    });
+    // for (var i = 0; i < attacker.power; i++) {
+    //   console.log(i);
+    //   var roll = 1;
+    // }
+    // var totalRoll = _(attacker.power).reduce(function(memo, num) {
+    //   console.log(i);
+    //   var roll = [1,2,3,4,5,6].randomElement();
+    // });
+    return totalRoll;
+  }
+});
+
+notices_on     = true;
+full_debugging = true;
+
+notice = function(message) {
+  if (notices_on) {
+    console.log(message);
+  }
+}
+
+debug = function(message) {
+  if (full_debugging) {
+    console.log(message);
+  }
+}
