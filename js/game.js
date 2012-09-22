2cf15f26b5eb1c725627302d802d879bd068778d
Darren Torpey <darren.torpey@gmail.com>
2012-09-22 15:28:04 -0400

    game files

---
+++ js/game.js
@@ -0,0 +1,213 @@
+// game.js for Perlenspiel 2.1
+
+/*
+Perlenspiel is a scheme by Professor Moriarty (bmoriarty@wpi.edu).
+Perlenspiel is Copyright © 2009-12 Worcester Polytechnic Institute.
+This file is part of Perlenspiel.
+
+Perlenspiel is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Perlenspiel is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You may have received a copy of the GNU Lesser General Public License
+along with Perlenspiel. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// This is a template for creating new Perlenspiel games
+// All of the functions below MUST exist, or the engine will stop and complain!
+
+// The following comment line is for JSLint. Don't remove it!
+
+/*global PS */
+
+// PS.Init ()
+// Initializes the game
+// This function normally includes a call to PS.GridSize (x, y)
+// where x and y are the desired dimensions of the grid
+var GLOBALS = {
+	boardHeight : 4,
+	boardWidth : 8
+};
+
+PS.Init = function ()
+{
+	"use strict";
+
+	// change to the dimensions you want
+
+	PS.GridSize ( GLOBALS.boardWidth, GLOBALS.boardHeight ); 
+	PS.GridBGColor(PS.COLOR_WHITE);
+	
+	initializeRandomBoard();
+
+    // Put any other init code here
+};
+
+function initializeRandomBoard() {
+	
+	//Generate team colors
+	var teamColors = [PS.COLOR_RED, PS.COLOR_BLUE, PS.COLOR_GREEN, PS.COLOR_VIOLET];
+	
+	//Generate a list of a strengths of units {1,1,2,3,...}
+	var unitStrengths = [1,1,2,3];
+	var unitStrengthStrings = ['1','1','2','3'];
+	
+	//Generate a list of all board positions
+	var boardPositions = [];
+	var counter = 0;
+	for (var i = 0 ; i < GLOBALS.boardHeight; i++)
+	{
+		for (var j = 0; j < GLOBALS.boardWidth; j++)
+		{
+			var num = i * GLOBALS.boardWidth + j;
+			boardPositions[num] = {x:j,y:i};
+		}
+	}
+	
+	//Scramble that list
+	boardPositions = _.shuffle(boardPositions);
+	
+	//Generate 4 teams with random areas
+	var numberOfTiles = GLOBALS.boardHeight * GLOBALS.boardWidth;
+	var strengthIndex = 0;
+	var colorIndex = 0;
+	for (var i = 0; i < numberOfTiles; i++)
+	{
+		var x = boardPositions[i].x;
+		var y = boardPositions[i].y;
+		PS.BeadColor(x, y, teamColors[colorIndex]);
+		PS.BeadGlyphColor(x, y, PS.COLOR_BLACK);
+		PS.BeadGlyph(x,y,unitStrengthStrings[strengthIndex]);
+		var data = PS.BeadData(x, y);
+		data.strength = unitStrengths[strengthIndex];
+		PS.BeadData(x, y, data);
+			
+		colorIndex++;
+		if (colorIndex > 3) {
+			colorIndex = 0;
+			strengthIndex++;
+			if (strengthIndex > 3) {
+				strengthIndex = 0;
+			}
+		}
+	}
+}
+
+// PS.Click (x, y, data)
+// This function is called whenever a bead is clicked
+// It doesn't have to do anything
+// x = the x-position of the bead on the grid
+// y = the y-position of the bead on the grid
+// data = the data value associated with this bead, 0 if none has been set
+
+PS.Click = function (x, y, data)
+{
+	
+	"use strict";
+	
+	// put code here for bead clicks
+};
+
+// PS.Release (x, y, data)
+// This function is called whenever a mouse button is released over a bead
+// It doesn't have to do anything
+// x = the x-position of the bead on the grid
+// y = the y-position of the bead on the grid
+// data = the data value associated with this bead, 0 if none has been set
+
+PS.Release = function (x, y, data)
+{
+	"use strict";
+
+	// Put code here for when the mouse button is released over a bead	
+};
+
+// PS.Enter (x, y, button, data)
+// This function is called whenever the mouse moves over a bead
+// It doesn't have to do anything
+// x = the x-position of the bead on the grid
+// y = the y-position of the bead on the grid
+// data = the data value associated with this bead, 0 if none has been set
+
+PS.Enter = function (x, y, data)
+{
+	"use strict";
+
+	// Put code here for when the mouse enters a bead	
+};
+
+// PS.Leave (x, y, data)
+// This function is called whenever the mouse moves away from a bead
+// It doesn't have to do anything
+// x = the x-position of the bead on the grid
+// y = the y-position of the bead on the grid
+// data = the data value associated with this bead, 0 if none has been set
+
+PS.Leave = function (x, y, data)
+{
+	"use strict";
+
+	// Put code here for when the mouse leaves a bead	
+};
+
+// PS.KeyDown (key, shift, ctrl)
+// This function is called whenever a key on the keyboard is pressed
+// It doesn't have to do anything
+// key = the ASCII code of the pressed key, or one of the following constants:
+// Arrow keys = PS.ARROW_UP, PS.ARROW_DOWN, PS.ARROW_LEFT, PS.ARROW_RIGHT
+// Function keys = PS.F1 through PS.F1
+// shift = true if shift key is held down, false otherwise
+// ctrl = true if control key is held down, false otherwise
+
+PS.KeyDown = function (key, shift, ctrl)
+{
+	"use strict";
+
+	// Put code here for when a key is pressed	
+};
+
+// PS.KeyUp (key, shift, ctrl)
+// This function is called whenever a key on the keyboard is released
+// It doesn't have to do anything
+// key = the ASCII code of the pressed key, or one of the following constants:
+// Arrow keys = PS.ARROW_UP, PS.ARROW_DOWN, PS.ARROW_LEFT, PS.ARROW_RIGHT
+// Function keys = PS.F1 through PS.F12
+// shift = true if shift key is held down, false otherwise
+// ctrl = true if control key is held down, false otherwise
+
+PS.KeyUp = function (key, shift, ctrl)
+{
+	"use strict";
+	
+	// Put code here for when a key is released	
+};
+
+// PS.Wheel (dir)
+// This function is called whenever the mouse wheel moves forward or backward
+// It doesn't have to do anything
+// dir = PS.FORWARD if mouse wheel moves forward, PS.BACKWARD if backward
+
+PS.Wheel = function (dir)
+{
+	"use strict";
+
+	// Put code here for when mouse wheel is moved
+};
+
+// PS.Tick ()
+// This function is called on every clock tick
+// if a timer has been activated with a call to PS.Timer()
+// It doesn't have to do anything
+
+PS.Tick = function ()
+{
+	"use strict";
+
+	// Put code here to handle clock ticks
+};
